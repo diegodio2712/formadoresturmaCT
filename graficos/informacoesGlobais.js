59ab88c3ea8ab4293398b7951a761a45d7e20797
diegodio2712 <145599668+diegodio2712@users.noreply.github.com>
2024-10-31 14:58:05 -0300

    .

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,9 +1,14 @@
-const url = "https://github.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/blob/Aula01/transporte/transporte-dados-globais.json ";
+const url= "https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/transporte/transporte-dados-globais.json"
 
-async function visualizarInformacoesGlobais() {
-    const resposta = await fetch(url);
-    const dados = await resposta.json();
+async function visualizarInformacoesGlobais(){
+    const resposta = await fetch (url);
+    let dados = await resposta.json();
     console.log(dados);
-}
 
+    const paragrafo = document.createElement("p");
+    paragrafo.classList.add("graficos-container__texto");
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo},</span> pessoas?`
+    const container = document.getElementById("graficos-container");
+    container.appendChild(paragrafo);
+}
 visualizarInformacoesGlobais();
